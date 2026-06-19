ad012b05ceef46a919b3f08a5ca24b611ca7ff60
Constantin Jacob <cj@tzeejay.com>
2026-06-19 18:13:55 +0200

    Move function into companion object for easier use

--- guardian/src/main/java/com/guardianconnect/GRDWireGuardConfiguration.kt
+++ guardian/src/main/java/com/guardianconnect/GRDWireGuardConfiguration.kt
@@ -12,60 +12,56 @@ import java.io.StringReader
 /*  A class that implements convenience functions to handle and create WireGuard configuration
     strings and objects for use at runtime or for export */
 class GRDWireGuardConfiguration {
-
     private val tag = GRDWireGuardConfiguration::class.java.simpleName
 
-    // Create a WireGuard configuration string from a set of function parameters.
-    fun getWireGuardConfigString(
-        grdCredential: GRDCredential,
-        dnsServersParam: String?,
-        appExceptionList: ArrayList<String>? = arrayListOf(),
-        excludeLANTraffic: Boolean = false
-    ): String {
-        var dnsServers = dnsServersParam
+	companion object {
+		// Create a WireGuard configuration string from a set of function parameters.
+		fun getWireGuardConfigString(grdCredential: GRDCredential,  dnsServersParam: String?, appExceptionList: ArrayList<String>? = arrayListOf(), excludeLANTraffic: Boolean = false): String {
+			var dnsServers = dnsServersParam
 
-        if (dnsServers.isNullOrEmpty()) {
-            dnsServers = "1.1.1.1, 1.0.0.1"
-        }
+			if (dnsServers.isNullOrEmpty()) {
+				dnsServers = "1.1.1.1, 1.0.0.1"
+			}
 
-        val appExceptions: String? = if (grdCredential.mainCredential == true && !appExceptionList.isNullOrEmpty()) {
-            if (appExceptionList.size > 1) {
-                appExceptionList.joinToString(", ")
-            } else {
-                appExceptionList.firstOrNull()
-            }
-        } else {
-            null
-        }
+			val appExceptions: String? = if (grdCredential.mainCredential == true && !appExceptionList.isNullOrEmpty()) {
+				if (appExceptionList.size > 1) {
+					appExceptionList.joinToString(", ")
+				} else {
+					appExceptionList.firstOrNull()
+				}
+			} else {
+				null
+			}
 
-        val allowedIPs = if (excludeLANTraffic) {
-            "0.0.0.0/5,8.0.0.0/7,11.0.0.0/8,12.0.0.0/6,16.0.0.0/4,32.0.0.0/3,64.0.0.0/2,128.0.0.0/3," +
-                    "160.0.0.0/5,168.0.0.0/6,172.0.0.0/12,172.32.0.0/11,172.64.0.0/10,172.128.0.0/9,173.0.0.0/8," +
-                    "174.0.0.0/7,176.0.0.0/4,192.0.0.0/9,192.128.0.0/11,192.160.0.0/13,192.169.0.0/16,192.170.0.0/15," +
-                    "192.172.0.0/14,192.176.0.0/12,192.192.0.0/10,193.0.0.0/8,194.0.0.0/7,196.0.0.0/6,200.0.0.0/5," +
-                    "208.0.0.0/4,224.0.0.0/3,::/1,8000::/2,c000::/3,e000::/4,f000::/5,f800::/6,fc00::/8,fe00::/7"
-        } else {
-            "0.0.0.0/0, ::/0"
-        }
+			val allowedIPs = if (excludeLANTraffic) {
+				"0.0.0.0/5,8.0.0.0/7,11.0.0.0/8,12.0.0.0/6,16.0.0.0/4,32.0.0.0/3,64.0.0.0/2,128.0.0.0/3," +
+						"160.0.0.0/5,168.0.0.0/6,172.0.0.0/12,172.32.0.0/11,172.64.0.0/10,172.128.0.0/9,173.0.0.0/8," +
+						"174.0.0.0/7,176.0.0.0/4,192.0.0.0/9,192.128.0.0/11,192.160.0.0/13,192.169.0.0/16,192.170.0.0/15," +
+						"192.172.0.0/14,192.176.0.0/12,192.192.0.0/10,193.0.0.0/8,194.0.0.0/7,196.0.0.0/6,200.0.0.0/5," +
+						"208.0.0.0/4,224.0.0.0/3,::/1,8000::/2,c000::/3,e000::/4,f000::/5,f800::/6,fc00::/8,fe00::/7"
+			} else {
+				"0.0.0.0/0, ::/0"
+			}
 
-        val configStringBuilder = StringBuilder().apply {
-            append("[Interface]")
-            append("\nPrivateKey = ${grdCredential.devicePrivateKey}")
-            append("\nAddress = ${grdCredential.IPv4Address}")
-            append("\nDNS = $dnsServers")
-            appExceptions?.let {
-                append("\nExcludedApplications = $it")
-            }
-            append("\n\n[Peer]")
-            append("\nPublicKey = ${grdCredential.serverPublicKey}")
-            append("\nAllowedIPs = $allowedIPs")
-            append("\nEndpoint = ${grdCredential.hostname}:51821")
-        }
+			val configStringBuilder = StringBuilder().apply {
+				append("[Interface]")
+				append("\nPrivateKey = ${grdCredential.devicePrivateKey}")
+				append("\nAddress = ${grdCredential.IPv4Address}")
+				append("\nDNS = $dnsServers")
+				appExceptions?.let {
+					append("\nExcludedApplications = $it")
+				}
+				append("\n\n[Peer]")
+				append("\nPublicKey = ${grdCredential.serverPublicKey}")
+				append("\nAllowedIPs = $allowedIPs")
+				append("\nEndpoint = ${grdCredential.hostname}:51821")
+			}
 
-        val configString = configStringBuilder.toString()
-        Log.d(tag, "Formatted WireGuard config: \n$configString")
-        return configString
-    }
+			val configString = configStringBuilder.toString()
+//			Log.d(tag, "Formatted WireGuard config: \n$configString")
+			return configString
+		}
+	}
 
     // Create a WireGuard configuration object from a set of function parameters.
     fun getWireGuardConfigObject(grdCredential: GRDCredential, dnsServersParam: String?): Config? {
