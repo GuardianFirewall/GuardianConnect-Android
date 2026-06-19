07dba5d92155da07ce2f8a2125e02b2cba1f18c5
Constantin Jacob <cj@tzeejay.com>
2026-06-19 19:55:47 +0200

    Cleanup API endpoint calls into distinctive groups & adopt SGW v1.4 APIs

--- guardian/src/main/java/com/guardianconnect/api/IApiCalls.kt
+++ guardian/src/main/java/com/guardianconnect/api/IApiCalls.kt
@@ -1,47 +1,90 @@
 package com.guardianconnect.api
 
 import com.google.gson.JsonObject
-import com.guardianconnect.model.api.NewVPNDevice
-import com.guardianconnect.model.*
 import com.guardianconnect.model.api.*
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.*
 
 interface IApiCalls {
-
+	//
+	// Secure Gateway endpoints
     @GET("/api/v1.3/server-status")
     fun getServerStatus(): Call<ResponseBody>
 
 	@GET("/api/v1.3/server-status/{device-id}")
 	fun getServerStatusForDeviceId(@Path("device-id") deviceId: String): Call<ResponseBody>
 
+	@POST("/api/v1.4/device-credentials")
+	fun createNewVPNDevice(
+		@Body requestData: @JvmSuppressWildcards MutableMap<String, Any>
+	): Call<ResponseBody>
+
+	@GET("/api/v1.4/device/{device-id}/verify-credentials")
+	fun verifyVPNCredentials(
+		@Path("device-id") deviceId: String,
+		@Header("grd-api-auth-token") apiAuthToken: String
+	): Call<ResponseBody>
+
+	@POST("/api/v1.4/device/{device-id}/invalidate-credentials")
+	fun invalidateVPNCredentials(
+		@Path("device-id") deviceId: String,
+		@Body requestData: @JvmSuppressWildcards MutableMap<String, Any>
+	): Call<ResponseBody>
+
+	@POST("/api/v1.4/device/{device-id}/alerts")
+	fun downloadAlerts(
+		@Path("device-id") deviceId: String,
+		@Body requestData: @JvmSuppressWildcards MutableMap<String, Any>
+	): Call<ResponseBody>
+
+	@GET("/api/v1.4/device/{device-id}/config/filters")
+	fun getDeviceFilterConfig(
+		@Path("device-id") deviceId: String,
+		@Header("grd-api-auth-token") apiAuthToken: String
+	): Call<ResponseBody>
+
+	@POST("/api/v1.4/device/{device-id}/config/filters")
+	fun setDeviceFilterConfig(
+		@Path("device-id") deviceId: String,
+		@Body requestData: JsonObject
+	): Call<ResponseBody>
+
+	@GET("/api/v1.4/device/{device-id}/config/rules")
+	fun getClientRules(
+		@Path("device-id") deviceId: String,
+		@Header("grd-api-auth-token") apiAuthToken: String
+	): Call<ResponseBody>
+
+	@POST("/api/v1.4/device/{device-id}/config/rules")
+	fun setClientRules(
+		@Path("device-id") deviceId: String,
+		@Body requestData: @JvmSuppressWildcards MutableMap<String, Any>
+	): Call<ResponseBody>
+
+	@GET("/api/v1.4/device/{device-id}/config/multihop")
+	fun getMultihopExitRegion(
+		@Path("device-id") deviceId: String,
+		@Header("grd-api-auth-token") apiAuthToken: String
+	): Call<ResponseBody>
+
+	@POST("/api/v1.4/device/{device-id}/config/multihop")
+	fun settMultihopExitRegion(
+		@Path("device-id") deviceId: String,
+		@Body requestData: @JvmSuppressWildcards MutableMap<String, Any>
+	): Call<ResponseBody>
+
+
+	//
+	// Connect API endpoints
+	@POST("/api/v1.2/subscriber-credential/create")
+	fun getSubscriberCredential(
+		@Body request: @JvmSuppressWildcards MutableMap<String, Any>
+	): Call<ResponseBody>
+
     @POST("/api/v1/users/sign-out")
     fun signOutUser(
         @Body signOutUserRequest: SignOutUserRequest
-    ): Call<ResponseBody>
-
-    @POST("/api/v1.3/device")
-    fun createNewVPNDevice(
-        @Body newVPNDevice: NewVPNDevice
-    ): Call<NewVPNDeviceResponse>
-
-    @POST("/api/v1.3/device/{deviceid}/verify-credentials")
-    fun verifyVPNCredentials(
-        @Path("deviceid") deviceid: String,
-        @Body requestData: @JvmSuppressWildcards MutableMap<String, Any>
-    ): Call<ResponseBody>
-
-    @POST("/api/v1.3/device/{deviceid}/invalidate-credentials")
-    fun invalidateVPNCredentials(
-        @Path("deviceid") deviceid: String,
-        @Body requestData: @JvmSuppressWildcards MutableMap<String, Any>
-    ): Call<ResponseBody>
-
-    @POST("/api/v1.2/device/{deviceid}/alerts")
-    fun downloadAlerts(
-        @Path("deviceid") deviceid: String,
-        @Body alerts: Alerts
     ): Call<ResponseBody>
 
     @GET("/api/v1/servers/all-server-regions")
@@ -60,11 +103,9 @@ interface IApiCalls {
     @GET("/api/v1.1/servers/timezones-for-regions")
     fun getListOfSupportedTimeZones(): Call<ResponseBody>
 
-    @POST("/api/v1.2/subscriber-credential/create")
-    fun getSubscriberCredential(
-        @Body request: @JvmSuppressWildcards MutableMap<String, Any>
-    ): Call<ResponseBody>
-
+    //
+	// Connect API endpoints
+	// 	Connect Subscriber
     @POST("/api/v1.3/partners/subscribers/new")
     fun createNewGRDConnectSubscriber(
         @Body request: @JvmSuppressWildcards Map<String, Any>
@@ -80,6 +121,19 @@ interface IApiCalls {
         @Body request: @JvmSuppressWildcards MutableMap<String, Any>
     ): Call<ResponseBody>
 
+	@POST("/api/v1.2/partners/subscriber/account-creation-state")
+	fun getAccountSignUpState(
+		@Body request: @JvmSuppressWildcards MutableMap<String, Any>
+	): Call<ResponseBody>
+
+	@POST("/api/v1.2/partners/subscriber/logout")
+	fun logoutConnectSubscriber(
+		@Body logoutConnectSubscriberRequest: MutableMap<String, Any>
+	): Call<ResponseBody>
+
+	//
+	// Connect API endpoints
+	// 	Connect Subscriber
     @POST("/api/v1.2/partners/subscriber/devices/add")
     fun addConnectDevice(
         @Body request: @JvmSuppressWildcards MutableMap<String, Any>
@@ -103,21 +157,5 @@ interface IApiCalls {
     @POST("/api/v1.2/partners/subscriber/device-reference")
     fun getConnectDeviceReference(
         @Body request: @JvmSuppressWildcards MutableMap<String, Any>
-    ): Call<ResponseBody>
-
-    @POST("/api/v1.2/partners/subscriber/account-creation-state")
-    fun getAccountSignUpState(
-        @Body request: @JvmSuppressWildcards MutableMap<String, Any>
-    ): Call<ResponseBody>
-
-    @POST("/api/v1.3/device/{deviceid}/config/filters")
-    fun setDeviceFilterConfig(
-        @Path("deviceid") deviceid: String,
-        @Body jsonObject: JsonObject
-    ): Call<ResponseBody>
-
-    @POST("/api/v1.2/partners/subscriber/logout")
-    fun logoutConnectSubscriber(
-        @Body logoutConnectSubscriberRequest: MutableMap<String, Any>
     ): Call<ResponseBody>
 }
